a27535eeb9f38e594d68b2611272d5994580bf4c
Muhammad Juan Akbar <mail@mjuan.me>
2016-11-05 21:11:14 +0100

    added project

---
+++ PimSuite/Models/UserMeta.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PimSuite.Models
+{
+    public class UserMeta
+    {
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+    }
+
+    [MetadataType(typeof(UserMeta))]
+    public partial class Users { }
+}
