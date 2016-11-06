10218a6f4b6ca461ea59d8c5ad17934f0e7ec8e6
Muhammad Juan Akbar <mail@mjuan.me>
2016-11-06 22:15:24 +0100

    added signalr dependency

---
+++ PimSuite/ChatHelper/SignalRConfig.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PimSuite.ChatHelper.SignalRConfig))]
+namespace PimSuite.ChatHelper
+{
+    public class SignalRConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
