7752e4c75e7d720028e66a0e925b91ba41d70d7b
Muhammad Juan Akbar <mail@mjuan.me>
2016-11-13 10:04:13 +0100

    cleanup db connections on app start

--- PimSuite/Global.asax.cs
+++ PimSuite/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using PimSuite.Models;
 
 namespace PimSuite
 {
@@ -11,11 +12,18 @@ namespace PimSuite
     {
         protected void Application_Start()
         {
+            CleanUpConnectionData();
             GlobalFilters.Filters.Add(new AuthorizeAttribute());
             AreaRegistration.RegisterAllAreas();
 //            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        private void CleanUpConnectionData()
+        {
+            var db = new PimSuiteDatabaseEntities();
+            db.Database.ExecuteSqlCommand("TRUNCATE TABLE [Connections]");
         }
     }
 }
